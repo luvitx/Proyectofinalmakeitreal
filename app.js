3c2c587d12b2dac34b8d3251930997b86695475a
luvitx <luvitx@gmail.com>
2023-08-03 18:48:30 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,386 @@
+
+const questions = [
+    {
+        question: "¿Cuál es el autor de la famosa obra Romeo y Julieta?",
+        optionA: "Charles Dickens",
+        optionB: "Jane Austen",
+        optionC: "Mark Twain",
+        optionD: "William Shakespeare",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "¿Cuál es el animal más rápido del mundo?",
+        optionA: "Águila",
+        optionB: "Guepardo",
+        optionC: "León",
+        optionD: "Elefante",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "¿Cuál es el símbolo químico del hierro?",
+        optionA: "He",
+        optionB: "Ir",
+        optionC: "H",
+        optionD: "Fe",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "¿En qué país se encuentra la famosa Gran Barrera de Coral?",
+        optionA: "Hawái",
+        optionB: "Filipinas",
+        optionC: "Australia",
+        optionD: "Maldivas",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "¿Cuál es el monte más alto del mundo?",
+        optionA: "Kilimanjaro",
+        optionB: "Aconcagua",
+        optionC: "Mont Blanc",
+        optionD: "Monte Everest",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "¿Cuál es el órgano encargado de bombear la sangre en nuestro cuerpo?",
+        optionA: "Corazón",
+        optionB: "Riñón",
+        optionC: "Hígado",
+        optionD: "Pulmón",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "¿Qué ciudad es famosa por sus canales y góndolas?",
+        optionA: "Ámsterdam",
+        optionB: "París",
+        optionC: "Venecia",
+        optionD: "Florencia",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "¿Quién fue el primer presidente de los Estados Unidos?",
+        optionA: "George Washington",
+        optionB: "Abraham Lincoln",
+        optionC: "Thomas Jefferson",
+        optionD: "Benjamin Franklin",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "¿Cuál es el país conocido como La cuna de la civilización?",
+        optionA: "Egipto",
+        optionB: "China",
+        optionC: "Grecia",
+        optionD: "Irak",
+        correctOption: "optionD"
+    },
+
+    {
+        question: `¿Cuál es el animal que representa a la marca Ferrari?`,
+        optionA: "Toro",
+        optionB: "León",
+        optionC: "Águila",
+        optionD: "Caballo",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "¿Cuál es el continente más grande del mundo?",
+        optionA: "Africa",
+        optionB: "América",
+        optionC: "Asia",
+        optionD: "Europa",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "¿Qué famoso científico formuló la teoría de la relatividad?",
+        optionA: "Albert Einstein",
+        optionB: "Isaac Newton",
+        optionC: "Galileo Galilei",
+        optionD: "Nikola Tesla",
+        correctOption: "optionA"
+    },
+
+
+    {
+        question: "¿En qué país se encuentra la famosa Torre Eiffel?",
+        optionA: "Italia",
+        optionB: "Francia",
+        optionC: "Alemania",
+        optionD: "España",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "¿Cuál es el país más poblado de América Latina?",
+        optionA: "México",
+        optionB: "Argentina",
+        optionC: "Colombia",
+        optionD: "Brasil",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "¿Cuál es el país que cuenta con el Taj Mahal como una de sus principales atracciones turísticas?",
+        optionA: "India",
+        optionB: "Tailandia",
+        optionC: "Egipto",
+        optionD: "Italia",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "¿Cuál es el río más largo del mundo?",
+        optionA: "Río Nilo",
+        optionB: "Río Yangtsé",
+        optionC: "Río Amazonas",
+        optionD: "Río Misisipi",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "¿Cuál es el resultado de sumar todos los ángulos internos de un triángulo?",
+        optionA: "180 grados",
+        optionB: "90 grados",
+        optionC: "270 grados",
+        optionD: "360 grados",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "¿Qué es lo que los mosquitos inyectan en nuestra piel cuando nos pican?",
+        optionA: "Agua",
+        optionB: "Veneno",
+        optionC: "Saliva",
+        optionD: "Sangre",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "¿Cuál es el ave que no puede volar?",
+        optionA: "Águila",
+        optionB: "Colibrí",
+        optionC: "Cóndor",
+        optionD: "Pingüino",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "¿Qué es lo que mide un termómetro?",
+        optionA: "Temperatura",
+        optionB: "Presión atmosférica",
+        optionC: "Humedad",
+        optionD: "Velocidad del viento",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "¿Qué famoso ratón lleva pantalones cortos y guantes blancos?",
+        optionA: "Jerry",
+        optionB: "Bugs Bunny",
+        optionC: "Minnie Mouse",
+        optionD: "Mickey Mouse",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "¿Cuál es el resultado de sumar todos los números de un dado de seis caras?",
+        optionA: "18",
+        optionB: "12",
+        optionC: "21",
+        optionD: "28",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "¿Qué famoso escritor creó al personaje de Sherlock Holmes?",
+        optionA: "J.K. Rowling",
+        optionB: "Sir Arthur Conan Doyle",
+        optionC: "Charles Dickens",
+        optionD: "Edgar Allan Poe",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "¿Qué famoso personaje vive en el País de las Maravillas?",
+        optionA: "Shrek",
+        optionB: "Superman",
+        optionC: "Alicia",
+        optionD: "Mario Bros",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "¿Cuál es el símbolo químico del oro?",
+        optionA: "Ag",
+        optionB: "Fe",
+        optionC: "Au",
+        optionD: "Pt",
+        correctOption: "optionC"
+    }
+
+]
+let shuffledQuestions = [] 
+
+
+function handleQuestions() { 
+    
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5).slice(0, 10);
+    document.getElementById('score-modal').style.display = "none"; 
+}
+
+
+
+let questionNumber = 1 
+let playerScore = 0  
+let wrongAttempt = 0 
+let indexNumber = 0 
+
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] 
+    const currentQuestionAnswer = currentQuestion.correctOption 
+    const options = document.getElementsByName("option"); 
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+          
+            correctOption = option.labels[0].id
+        }
+    })
+
+   
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+   
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++ 
+            indexNumber++ 
+           
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++ 
+            indexNumber++
+         
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+
+function handleNextQuestion() {
+    checkForAnswer() 
+    unCheckRadioButtons()
+  
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+function resetGame() {
+    questionNumber = 1;
+    playerScore = 0;
+    wrongAttempt = 0;
+    indexNumber = 0;
+    shuffledQuestions = [];
+    NextQuestion(0)
+}
+
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    
+    if (playerScore <= 3) {
+        remark = "Sigue intentando."
+        remarkColor = "red"
+    }
+    else if (playerScore >= 4 && playerScore < 7) {
+        remark = "Lo puedes hacer mejor, sigue intentando."
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 7) {
+        remark = "Buen trabajo."
+        remarkColor = "green"
+    }
+    const playerGrade = (playerScore / 10) * 100
+
+    
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex";
+
+}
+
+
+function closeScoreModal() {
+    document.getElementById('score-modal').style.display = "none"; 
+  
+    resetGame();
+    resetOptionBackground();
+  
+   ;
+}
+
